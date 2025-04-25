dc42812690fa1e68debe747c505472a61f5921db
T-H-10 <t0534198344@gmail.com>
2025-04-25 04:33:05 +0300

    add header component

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,25 @@
+import { Component, signal } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule
+  ],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+  showMenu = signal(false);
+  toggleMenu() {
+    this.showMenu.set(!this.showMenu());
+  }
+
+}
