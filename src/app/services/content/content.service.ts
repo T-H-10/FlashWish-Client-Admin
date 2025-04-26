3a61b2682879c374d3fd400579dca11f09d88e44
T-H-10 <t0534198344@gmail.com>
2025-04-27 01:36:04 +0300

    template and content service

--- src/app/services/content/content.service.ts
+++ src/app/services/content/content.service.ts
@@ -1,9 +1,35 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { Observable } from 'rxjs';
+import { Content } from '../../models/content.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContentService {
+  private apiUrl:string;
+  constructor(private http: HttpClient) {
+    this.apiUrl = environment.apiUrl+'/api/GreetingMessages';
+  }
 
-  constructor() { }
+  getAllContent():Observable<Content[]> {
+    return this.http.get<Content[]>(this.apiUrl);
+  }
+
+  getContentById(id: number): Observable<Content> {
+    return this.http.get<Content>(`${this.apiUrl}/${id}`);
+  }
+
+  addContent(content: Content): Observable<Content> {
+    return this.http.post<Content>(this.apiUrl, content);
+  }
+
+  updateContent(content: Content): Observable<Content> {
+    return this.http.put<Content>(`${this.apiUrl}/${content.textID}`, content);
+  }
+
+  deleteContent(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
 }
--- src/app/services/template/template.service.ts
+++ src/app/services/template/template.service.ts
@@ -1,9 +1,35 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { Template } from '../../models/template.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TemplateService {
+  private apiUrl: string;
+  constructor(private http: HttpClient) {
+    this.apiUrl = environment.apiUrl+'/api/Template';
+  }
 
-  constructor() { }
+  getAllTemplates(): Observable<Template[]> {
+    return this.http.get<Template[]>(this.apiUrl);
+  }
+
+  getTemplateById(id: number): Observable<Template> {
+    return this.http.get<Template>(`${this.apiUrl}/${id}`);
+  }
+
+  addTemplate(template: Template): Observable<Template> {
+    return this.http.post<Template>(this.apiUrl, template);
+  }
+
+  updateTemplate(template: Template): Observable<Template> {
+    return this.http.put<Template>(`${this.apiUrl}/${template.templateID}`, template);
+  }
+
+  deleteTemplate(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
 }
